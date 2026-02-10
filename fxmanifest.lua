8cbc2f1b22029a561938ece471a37b26593dda32
YusuDiscord <141464219+YusuDiscord@users.noreply.github.com>
2026-02-10 21:50:28 +0100

    Version 1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GDEV Studio'
+description 'Système de Notifications'
+version '1.0.1'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+}
+
+files {
+    'html/index.html',
+    'html/script/index.js',
+}
